140ae72e542a240b8a34c25fdf7907e66b2aa649
Prakhar Lad <46927944+prakharlad123@users.noreply.github.com>
2022-06-30 17:39:20 +0530

    Create Arithmetic Number.cpp

---
+++ Love Babbar 450 DSA/Searching and Sorting/Arithmetic Number.cpp
@@ -0,0 +1,16 @@
+class Solution{
+public:
+    int inSequence(int A, int B, int C){
+        if(C == 0) {
+            if(A == B) return 1;
+            return 0;
+        }
+        else {
+            if((B-A)/C < 0) return 0;
+            else {
+                if((B-A) % C == 0) return 1;
+                else return 0;
+            }
+        }
+    }
+};
