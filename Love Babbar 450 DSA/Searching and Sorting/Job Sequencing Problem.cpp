0ac1dccf7b99d875b68f353b80871de8f6e7be5f
Prakhar Lad <46927944+prakharlad123@users.noreply.github.com>
2022-07-03 01:14:12 +0530

    Update Job Sequencing Problem.cpp

--- Love Babbar 450 DSA/Searching and Sorting/Job Sequencing Problem.cpp
+++ Love Babbar 450 DSA/Searching and Sorting/Job Sequencing Problem.cpp
@@ -1,45 +1,41 @@
 class Solution 
 {
     public:
+    static bool cmp(Job a, Job b) {
+        return a.profit > b.profit;
+    }
+    
     //Function to find the maximum profit and the number of jobs done.
-    vector<int> JobScheduling(Job arr[], int n) 
-    { 
-        vector<int> ans;
-        vector<pair<int, int>> jobs;
-        int maxDead = 0;
-        
-        for(int i = 0; i < n; i++) {
-            maxDead = max(maxDead, arr[i].dead);
-            jobs.push_back({arr[i].profit, arr[i].dead});
-        }
-        
-        sort(jobs.begin(), jobs.end());
-        reverse(jobs.begin(), jobs.end());
-        vector<bool> comp(maxDead, false);
-        int profit = 0, comp_jobs = 0;
-        
-        for(int i = 0; i < n; i++) {
-            int d = jobs[i].second;
-            if(comp[d-1] == false) {
-                comp[d-1] = true;
-                comp_jobs++;
-                profit += jobs[i].first;
-            }
-            else {
-                for(int j = d-2; j >= 0; j--) {
-                    if(comp[j] == false) {
-                        comp[j] = true;
-                        comp_jobs++;
-                        profit += jobs[i].first;
-                        break;
-                    }
-                }
-            }
-        }
-        
-        ans.push_back(comp_jobs);
-        ans.push_back(profit);
-        
-        return ans;
+    vector<int> JobScheduling(Job arr[], int n) { 
+       sort(arr, arr+n, cmp);
+       
+       int maxDead = arr[0].dead;
+       for(int i = 1; i < n; i++) {
+           maxDead = max(maxDead, arr[i].dead);
+       }
+       
+       vector<int> days(maxDead+1, -1);
+       
+       int jobs = 0, jobProfit = 0;
+       
+       for(int i = 0; i < n; i++) {
+           if(days[arr[i].dead] == -1) {
+               jobProfit += arr[i].profit;
+               jobs++;
+               days[arr[i].dead] = 1;
+           }
+           else{
+               for(int j = arr[i].dead-1; j >= 1; j--) {
+                   if(days[j] == -1) {
+                       jobProfit += arr[i].profit;
+                       jobs++;
+                       days[j] = 1;
+                       break;
+                   }
+               }
+           }
+       }
+       
+       return {jobs, jobProfit};
     } 
 };
