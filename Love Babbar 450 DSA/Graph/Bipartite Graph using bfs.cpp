8bd853268f48c1d40789c1df9746ed652a22c5b3
Prakhar Lad <46927944+prakharlad123@users.noreply.github.com>
2022-07-14 17:48:11 +0530

    Create Bipartite Graph using bfs.cpp

---
+++ Love Babbar 450 DSA/Graph/Bipartite Graph using bfs.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool checkBipartite(int src, vector<int> adj[], vector<int>& color) {
+        queue<int> q;
+        q.push(src);
+        color[src] = 1;
+        
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            
+            for(auto it : adj[node]) {
+                if(color[it] == -1) {
+                    color[it] = 1-color[node];
+                    q.push(it);
+                }
+                else if(color[it] == color[node]) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+
+	bool isBipartite(int V, vector<int>adj[]){
+	    vector<int> color(V+1, -1);
+	    
+	    for(int i = 0; i < V; i++) {
+	        if(color[i] == -1) {
+	            if(!checkBipartite(i, adj, color)) {
+	                return false;
+	            }
+	        }
+	    }
+	    
+	    return true;
+	}
+
+};
